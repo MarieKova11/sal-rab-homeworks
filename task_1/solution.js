11e232749d6415b99588127d326cb492037d31b2
Maria Volkova <itachi-030198@mail.ru>
2023-04-22 12:54:30 +0500

    Task 1

--- task_1/solution.js
+++ task_1/solution.js
@@ -2,20 +2,16 @@ function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
 
-    // Задание №1.1. Оповещение о добавлении в корзину
+    let message;
 
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
+    message = productName;
+    message = productName + " ";
+    message = productName + " за";
+    message = productName + " за" + productPrice;
+    message = productName + " за" + productPrice + " ";
+    message = productName + " за" + productPrice + " теперь в корзине!";
 
-    // Конец решения задания №1.1.
-
+    console.log(message);
     return message;
 }
 
@@ -24,11 +20,9 @@ function addInCartCountChange(value) {
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
 
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
-
-    // Конец решения задания №1.2.
+    let newValue;
+    newValue = oldValue;
+    newValue = oldValue + 1;
 
     return newValue;
 }
@@ -38,12 +32,9 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
-
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
-
-    // Конец решения задания №1.3.
+    let newSum = $oldSum + $difference
+    let newSumText = $newSum + '₽'
+    newSumText = `${newSum} ₽`;
 
     return newSumText;
 }
