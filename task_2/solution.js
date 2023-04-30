ef68728d9552bc7b2e3214e65d6a455beed8fa5d
MarieKova11 <118646673+MarieKova11@users.noreply.github.com>
2023-04-30 16:37:06 +0500

    Update solution.js

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,11 +5,12 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
+    let shippingSum;
     if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
       }  
     
-      else  ((productsSum > 0) & (productsSum < freeShippingMinSum)) {
+    if (productsSum > 0 && productsSum < freeShippingMinSum) {
         shippingSum = shippingPrice;
         }
 
