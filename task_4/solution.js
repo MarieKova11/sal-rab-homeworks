b8f589bb0d4153ef8bd21ad340f405b303d3d46b
Maria Volkova <itachi-030198@mail.ru>
2023-05-07 11:26:21 +0500

    4task

--- task_4/solution.js
+++ task_4/solution.js
@@ -9,10 +9,11 @@ function parseProducts(json) {
 function renderProductsCards(json) {
     clearProducts ()
     let products = parseProducts(json);
-    return products;} 
+    return products;
 
     let lenght = products.length
     for (let i = 0; i < length; i += 1) {
     addProduct(products[i]);
      }
+   } 
      
